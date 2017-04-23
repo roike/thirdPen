4547e550b2ecce57812e90df835bf29dcf54d950
ryuji oike <ryuji.oike@gmail.com>
2017-04-23 15:50:03 +0900

    delete commit

--- dev_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# project: Third-pen
-#dev_appserver.py --enable_sendmail=yes --datastore_path=/Users/ryuji/GoogleDrive/gaestorage/thirdpen app.yaml
-dev_appserver.py --enable_sendmail=yes --port=8083 dispatch.yaml app.yaml task.yaml
-#dev_appserver.py --enable_sendmail=yes --port=8083 app.yaml
