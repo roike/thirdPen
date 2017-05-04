46af738e8684d0626261a8df012c6bb9a17877e8
ryuji.oike <ryuji.oike@gmail.com>
2017-05-04 01:28:54 +0000

    fixed static_files

--- main.py
+++ main.py
@@ -16,6 +16,30 @@ ALLOW_ANCHOR = ['home', 'newist', 'blog', 'contact']
 #projectID
 PROJECTID = 'vpwboard'
 #---static_file section--------------------------------
+@app.route('/test')
+def hello():
+    return static_file('test.html', root='vpwboard/static')
+
+@app.route('/icon.png')
+def favicon():
+    return static_file('icon.png', root='vpwboard/static/images')
+
+@app.route('/images/<imgname>')
+def serve_img(imgname):
+    return static_file(imgname, root='vpwboard/static/images')
+
+@app.route('/css/<cssname>')
+def serve_css(cssname):
+    return static_file(cssname, root='vpwboard/static/css')
+
+@app.route('/js/<jsname>')
+def serve_js(jsname):
+    return static_file(jsname, root='vpwboard/static/js')
+
+@app.route('/js/lib/<libname>')
+def serve_jslib(libname):
+    return static_file(libname, root='vpwboard/static/js/lib')
+
 #urlを直接入力する場合(bookmarkも同じ)もここに入る
 #anchorがなければtemplateでページ不在を知らせる<--未実装
 @app.route('/')
@@ -23,7 +47,7 @@ PROJECTID = 'vpwboard'
 @app.route('/<anchor>/<:re:.*>')
 def init_anchor(anchor='home'):
     if anchor in ALLOW_ANCHOR:
-        return static_file('pen.html', root='./static')
+        return static_file('pen.html', root='vpwboard/static')
 
     abort(404, 'Sorry, Nothing at this URL.')
 
